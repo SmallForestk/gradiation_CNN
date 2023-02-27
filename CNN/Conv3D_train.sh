b33290e6e9224f6fdaca5585ede3ceffcc322828
KOBAYASHI Keisuke <kobayashik@he03.he>
2023-02-27 12:55:46 +0900

    remove

--- CNN/Conv3D_train.sh
+++ CNN/Conv3D_train.sh
@@ -6,9 +6,4 @@ echo "execute Learning Rate=10^-3"
 python ConvNN_3D_h5.py train_dataset 3 200 pi
 python ../messege.py "end Learning Rate=10^-3"
 
-# python ../messege.py start Learning Rate=10^-4
-# echo "execute Learning Rate=10^-4"
-# python ConvNN_3D_h5.py train_dataset 4 400 pi 94720
-# python ../messege.py "end Learning Rate=10^-4"
-
 rm /mnt/scratch/kobayashik/hitmap.h5
