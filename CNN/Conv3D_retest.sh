bbe45424adb901e5078b33c56505dd22ab497efd
KOBAYASHI Keisuke <kobayashik@he03.he>
2023-02-27 13:07:02 +0900

    remove

--- CNN/Conv3D_retest.sh
+++ CNN/Conv3D_retest.sh
@@ -1,7 +1,3 @@
-python ../messege.py "start_retest"
 cp ../CNN_3cm/train_dataset/hitmap3D/hitmap.h5 /mnt/scratch/kobayashik
 python ConvNN_3D_h5retest.py 3 79 pi
-# python ConvNN_3D_h5retest.py 4 399 pi
-# python ConvNN_3D_h5retest.py 5 167 pi
 rm /mnt/scratch/kobayashik/hitmap.h5
-python ../messege.py "finish_retest"
