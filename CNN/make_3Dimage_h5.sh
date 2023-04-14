28f8858bc1ec8bbb236ee619f62c8f4c9cfda0bf
KOBAYASHI Keisuke <kobayashik@he03.he>
2023-04-14 20:52:29 +0900

    change input information

--- CNN/make_3Dimage_h5.sh
+++ CNN/make_3Dimage_h5.sh
@@ -1,7 +1,7 @@
-python make_3Dhitmap_h5.py train_dataset pi 90 0 90 0 48 0 30 30
+python make_3Dhitmap_h5.py ./pi_train.root 90 0 90 0 48 0 30 30
 for i in `seq 1 15`
 do
     energy=`expr $i \* 2`
     echo "execute in Cnn_${energy}GeV"
-    python make_3Dhitmap_h5.py "test_dataset/data_${energy}GeV" pi 90 0 90 0 48 0 30 30
+    python make_3Dhitmap_h5.py "./pi_${energy}GeV.root" 90 0 90 0 48 0 30 30
 done
